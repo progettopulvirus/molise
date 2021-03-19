4184a84824e1e0ce9477fc85977380c3174c1480
guidofioravanti <guidofioravanti@yahoo.it>
2021-03-19 15:25:36 +0100

    Introdotte serie complete solo per il 2020

--- R/nox.R
+++ R/nox.R
@@ -1,6 +1,6 @@
-#' Tibble con i dati di nox per 1 stazioni della regione MOLISE
+#' Tibble con i dati di nox per 4 stazioni della regione MOLISE
 #'
-#' @format Un tibble con 8 colonne e 2738 osservazioni
+#' @format Un tibble con 8 colonne e 10896 osservazioni
 #'
 #' @usage
 #' nox
--- R/o3_max_h_d.R
+++ R/o3_max_h_d.R
@@ -1,6 +1,6 @@
-#' Tibble con i dati di o3_max_h_d per 1 stazioni della regione MOLISE
+#' Tibble con i dati di o3_max_h_d per 2 stazioni della regione MOLISE
 #'
-#' @format Un tibble con 8 colonne e 2738 osservazioni
+#' @format Un tibble con 8 colonne e 5476 osservazioni
 #'
 #' @usage
 #' o3_max_h_d
